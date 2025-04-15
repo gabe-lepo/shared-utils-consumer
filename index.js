1053fa8b40c1494aad809dd702e3c822fb98679f
gabe-lepo <gabelepoutre@gmail.com>
2025-04-15 01:23:44 -0400

    it works

---
+++ index.js
@@ -0,0 +1,3 @@
+const { clamp2 } = require("@gabe-lepo/shared-utils-test")
+
+console.log(`-1 clamped is: ${clamp2(0, 100, -1)}`)
--- index.mjs
+++
@@ -1,4 +0,0 @@
-import { clamp } from "@gabe-lepo/shared-utils-test";
-
-console.log(`-1 clamped is: ${clamp(0, 100, -1)}`)
-console.log(`5000 clamped is: ${clamp(0, 100, 5000)}`)
